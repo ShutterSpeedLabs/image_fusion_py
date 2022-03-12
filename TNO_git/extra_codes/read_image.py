51474be9a626451b01a5759453b136d439dcb667
kisna-aryan <testseries0abc@gmail.com>
2022-03-12 17:03:25 +0530

    init

---
+++ TNO_git/extra_codes/read_image.py
@@ -0,0 +1,18 @@
+import cv2
+
+
+filename = r'./images/IR_18rad.bmp'
+
+img_grayscale = cv2.imread(filename,0) 
+
+# The function cv2.imshow() is used to display an image in a window.
+
+cv2.imshow('graycsale image',img_grayscale)
+
+# waitKey() waits for a key press to close the window and 0 specifies indefinite loop
+
+cv2.waitKey(0)
+
+# cv2.destroyAllWindows() simply destroys all the windows we created.
+
+cv2.destroyAllWindows()
