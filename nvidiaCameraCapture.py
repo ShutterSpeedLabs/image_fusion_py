5e818559fc5b0c9fc8a4ba9c56733342dd79c8b9
Kisna Aryan <52367760+kisna-aryan@users.noreply.github.com>
2022-08-07 14:26:25 +0530

    Create nvidiaCameraCapture.py

---
+++ nvidiaCameraCapture.py
@@ -0,0 +1,26 @@
+# import the opencv library
+import cv2
+GSTREAMER_PIPELINE = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=(string)NV12, framerate=30/1 ! nvvidconv flip-method=0 ! video/x-raw, width=1280, height=720, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
+
+# define a video capture object
+vid = cv2.VideoCapture(GSTREAMER_PIPELINE, cv2.CAP_GSTREAMER)
+
+while(True):
+	
+	# Capture the video frame
+	# by frame
+	ret, frame = vid.read()
+
+	# Display the resulting frame
+	cv2.imshow('frame', frame)
+	
+	# the 'q' button is set as the
+	# quitting button you may use any
+	# desired button of your choice
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
